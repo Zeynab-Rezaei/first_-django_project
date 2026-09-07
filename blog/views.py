ef8b1e5dd6bde290dda88fd470d309c58987aa65
zeinab rezaei <zeynabrezaei1cs@example.com>
2026-09-07 15:44:44 +0330

    enhance blog functionality   and add category field

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html',context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request,pid):
+    # برای بهینه کردن کوئری می تونیم از این روش پایین که در دوخط نوشته شده هم استفاده کنیم.
+    # posts = Post.objects.filter(status=1)
+    # post = get_object_or_404(posts,pk=pid, status=1)
+    post = get_object_or_404(Post,pk=pid, status=1)
+    context = {'post':post}
+    return render(request, 'blog/blog-single.html', context)
+
+def test(request,pid):
+    # post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post,pk=pid)
+    context = {'post':post}
+    return render(request, 'test.html', context)
