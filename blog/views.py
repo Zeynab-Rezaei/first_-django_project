688a7221eb970b509e1ead69dd159e97dfd54e71
zeinab rezaei <zeynabrezaei1cs@example.com>
2026-09-11 11:20:15 +0330

    add category and author filters for posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
-def blog_view(request):
+# روش اول
+# def blog_view(request,cat_name=None,author_username=None):
+#     posts = Post.objects.filter(status=1)
+#     if cat_name:
+#         posts = posts.filter(category__name=cat_name)
+#     if author_username:
+#         posts = posts.filter(author__username = author_username)
+#     context = {'posts':posts}
+#     return render(request, 'blog/blog-home.html',context)
+
+# روش دوم که روش بهتر و تمیزتری هست
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html',context)
 
@@ -16,3 +31,9 @@ def blog_single(request,pid):
 
 def test(request):
     return render(request,'test.html')
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
