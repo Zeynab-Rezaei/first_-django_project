c3319e5b039cd0ea1ddacf822c56837413cbc1cf
zeinab rezaei <zeynabrezaei1cs@example.com>
2026-09-20 08:15:15 +0330

    add Taggit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from taggit.models import Tag
 # روش اول
 # def blog_view(request,cat_name=None,author_username=None):
 #     posts = Post.objects.filter(status=1)
@@ -18,6 +19,10 @@ def blog_view(request,**kwargs):
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username = kwargs['author_username'])
+    if kwargs.get('tag_name') != None:
+        posts = posts.filter(tags__name__in=[kwargs['tag_name']])
+    tags = Tag.objects.all()
+
     posts = Paginator(posts,3)
     try:    
         page_number = request.GET.get('page')
@@ -26,7 +31,9 @@ def blog_view(request,**kwargs):
         posts = posts.get_page(1)
     except EmptyPage:
         posts = posts.get_page(1)
-    context = {'posts':posts}
+        
+    context = {'posts':posts, 'tags':tags,}
+
     return render(request, 'blog/blog-home.html',context)
 
 def blog_single(request,pid):
